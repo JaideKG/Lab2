9f35b2fff6cf5d5d455c21748a1b93565797b6d7
Jaide King-Griffin <jaidekg@gmail.com>
2018-04-24 15:30:13 -0400

    first commit

---
+++ Lab Number 2/Lab Number 2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Number_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            //Get Two Input Numbers
+            Console.WriteLine("Welcome to the Grand Circus' Room Detail Generator!");
+            Console.WriteLine("Please enter the length");
+            double length = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Please enter the width");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the height");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("area");
+            double area = (length * width) * 2;
+            Console.WriteLine(area);
+
+            Console.WriteLine("perimeter");
+            double perimeter = (width * 2) + (length * 2);
+            Console.WriteLine(perimeter);
+
+            Console.WriteLine("volume");
+            double volume = width * length * height;
+            Console.WriteLine(volume);
+
+            Console.WriteLine("\nContinue? (yes/no)");
+            string response;
+            response = Console.ReadLine();
+
+            
+            if (response == "yes")
+            {
+                Console.WriteLine("");
+            }
+
+            else { };
+
+          
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab Number 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Number_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get Two Input Numbers
+            Console.WriteLine("Please enter the length");
+            String input1 = Console.ReadLine();
+            Console.WriteLine("Please enter the width");
+            String input2 = Console.ReadLine();
+
+            //int length1 = int.Parse(input1);
+            //int width1 = int.Parse(input2);
+            //int total = length1 + width1;
+            int perimeter = input1[0] + input2[0];
+            int area = input1[1] + input2[1];
+            int ones = input1[2] + input2[2];
+            if (((perimeter == input1[0] + input2[0]) && (area == input1[1] + input2[2])))
+            {
+                Console.WriteLine("True");
+
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadLine();
+        }
+    }
+}
